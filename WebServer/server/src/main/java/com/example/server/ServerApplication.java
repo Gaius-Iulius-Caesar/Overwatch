13c104053ad71c93655095068a98c0744f343f02
Wu Sai <wusai.ustc@outlook.com>
2022-10-27 10:44:17 +0800

    注释优化

--- WebServer/server/src/main/java/com/example/server/ServerApplication.java
+++ WebServer/server/src/main/java/com/example/server/ServerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-
+// 无数据源启动
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class ServerApplication {
 
